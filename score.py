c3b1a664457d52c070805c0e97ffb1ec89ffd43d
aurion-cloud <aurion.howard@cloudreach.com>
2023-06-19 13:06:19 -0500

    sum updates

---
+++ score.py
@@ -0,0 +1,22 @@
+
+
+
+scores= [75, 82, 92, 68, 78]
+print(scores)
+
+avg = sum(scores)/len(scores)
+print(avg)
+
+# if score >= 90:
+#     grade = "A"
+# elif score >= 80:
+#     grade = "B"
+# elif score >= 70:
+#     grade = "C"
+# elif score >= 60:
+#     grade = "D"
+# else:
+#     grade = "F"
+
+
+
