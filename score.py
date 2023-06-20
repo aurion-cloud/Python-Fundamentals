768a8bc9e28dfb080b2ac9043265c6407963fea1
aurion-cloud <aurion.howard@cloudreach.com>
2023-06-19 22:38:04 -0500

    completed

--- score.py
+++ score.py
@@ -7,16 +7,15 @@ print(scores)
 avg = sum(scores)/len(scores)
 print(avg)
 
-# if score >= 90:
-#     grade = "A"
-# elif score >= 80:
-#     grade = "B"
-# elif score >= 70:
-#     grade = "C"
-# elif score >= 60:
-#     grade = "D"
-# else:
-#     grade = "F"
-
+if avg>=90:
+    print("Your Grade is A")
+elif avg>=80 and avg<=89:
+    print("Your Grade is B")
+elif avg>=70 and avg<=79:
+    print("Your Grade is a C")
+elif avg>=60 and avg<=69:
+    print("Your grade is a D")
+elif avg<60:
+    print("Your grade is a F")
 
 
